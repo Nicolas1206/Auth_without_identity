e92ac185aefbd7cbee0fb49066e5dfd3fd34522d
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-10-09 17:15:55 +0200

    auth working

---
+++ UserSignupLogin/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserSignupLogin.Models;
+
+namespace UserSignupLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBuserSignupLoginEntities1 db = new DBuserSignupLoginEntities1();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.TBLUserInfoes.ToList());
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(TBLUserInfo tBLUserInfo)
+        {
+            if (db.TBLUserInfoes.Any(x=>x.UsernameUs == tBLUserInfo.UsernameUs))
+            {
+                ViewBag.Notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.TBLUserInfoes.Add(tBLUserInfo);
+                db.SaveChanges();
+
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TBLUserInfo tBLUserInfo)
+        {
+            var checkLogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tBLUserInfo.UsernameUs)&& x.PasswordUs.Equals(tBLUserInfo.PasswordUs)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Wrong username/password";
+            }
+            return View();
+        }
+    }
+}
---
+++ UserSignupLogin/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales sur un assembly sont contrôlées via ce
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("UserSignupLogin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserSignupLogin")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("48bc0db6-c32a-4e49-86e7-ed075c911234")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Major Version
+//      Version mineure
+//      Build Number
+//      Revision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
+// en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
